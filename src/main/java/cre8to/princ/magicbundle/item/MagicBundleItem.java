d9775fa5e778e04ba463d440ade4bb9e26d6457f
PrinceParshia <164875986+PrinceParshia@users.noreply.github.com>
2025-04-09 11:53:42 +0600

    Update MagicBundleItem.java

--- src/main/java/cre8to/princ/magicbundle/item/MagicBundleItem.java
+++ src/main/java/cre8to/princ/magicbundle/item/MagicBundleItem.java
@@ -5,19 +5,14 @@ import net.minecraft.component.DataComponentTypes;
 import net.minecraft.component.type.BundleContentsComponent;
 import net.minecraft.component.type.NbtComponent;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.BundleItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtElement;
-import net.minecraft.nbt.NbtInt;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.text.Text;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
-import org.jetbrains.annotations.Nullable;
-
-import java.util.Optional;
 
 public class MagicBundleItem extends BundleItem {
     public static final String TIME_TAG = "invTime";
@@ -28,7 +23,7 @@ public class MagicBundleItem extends BundleItem {
     }
 
     @Override
-    public void inventoryTick(ItemStack stack, ServerWorld world, Entity entity, @Nullable EquipmentSlot slot) {
+    public void inventoryTick(ItemStack stack, World world, Entity entity, int slot, boolean selected) {
         if (world.isClient) {
             return;
         }
@@ -36,20 +31,20 @@ public class MagicBundleItem extends BundleItem {
         PlayerEntity player = MinecraftClient.getInstance().player;
         Random random = world.getRandom();
         NbtComponent component = stack.get(DataComponentTypes.CUSTOM_DATA);
-        BundleContentsComponent bundleContentsComponent = stack.get(DataComponentTypes.BUNDLE_CONTENTS);
+        BundleContentsComponent bundleContentsComponent = (BundleContentsComponent) stack.get(DataComponentTypes.BUNDLE_CONTENTS);
         if (component == null) {
             return;
         }
 
         if (player != null) {
             stack.set(DataComponentTypes.CUSTOM_DATA, component.apply(compound -> {
-                if (!compound.contains(MAX_TIME_TAG)) {
-                    compound.putInt(MAX_TIME_TAG, random.nextBetween(200, 600));
+                if (!compound.contains(MAX_TIME_TAG, NbtElement.INT_TYPE)) {
+                    compound.putInt(MAX_TIME_TAG, random.nextBetween(2500, 6100));
                 }
 
-                if (compound.contains(TIME_TAG)) {
-                    int invTime = compound.getInt(TIME_TAG).orElse(0) + 1;
-                    int maxTimeTag = compound.getInt(MAX_TIME_TAG).orElse(0);
+                if (compound.contains(TIME_TAG, NbtElement.INT_TYPE)) {
+                    int invTime = compound.getInt(TIME_TAG) + 1;
+                    int maxTimeTag = compound.getInt(MAX_TIME_TAG);
 
                     if (invTime >= (maxTimeTag - 100)) {
                         if (invTime % 20 == 0) {
@@ -84,8 +79,8 @@ public class MagicBundleItem extends BundleItem {
                                 ItemStack droppedStack = new ItemStack(Items.USED_MAGIC_BUNDLE);
                                 droppedStack.set(DataComponentTypes.BUNDLE_CONTENTS, bundleContentsComponent);
                                 player.sendMessage(Text.of("<MagicBundle> What a Luck!"), false);
-                                entity.dropStack(world, droppedStack);
-                                entity.dropStack(world, droppedStack.copy());
+                                entity.dropStack((ServerWorld) world, droppedStack);
+                                entity.dropStack((ServerWorld) world, droppedStack.copy());
                             }
                         }
 
