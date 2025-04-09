a5bb980556cdc16f00757d7a76426478bedc3668
PrinceParshia <164875986+PrinceParshia@users.noreply.github.com>
2025-04-09 11:54:44 +0600

    Update LuckyBundleItem.java

--- src/main/java/cre8to/princ/magicbundle/item/LuckyBundleItem.java
+++ src/main/java/cre8to/princ/magicbundle/item/LuckyBundleItem.java
@@ -4,18 +4,19 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.component.DataComponentTypes;
 import net.minecraft.component.type.BundleContentsComponent;
 import net.minecraft.component.type.NbtComponent;
-import net.minecraft.entity.*;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LightningEntity;
+import net.minecraft.entity.SpawnReason;
 import net.minecraft.entity.mob.CreeperEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.BundleItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtElement;
-import net.minecraft.nbt.NbtInt;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.text.Text;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,7 +30,7 @@ public class LuckyBundleItem extends BundleItem {
     }
 
     @Override
-    public void inventoryTick(ItemStack stack, ServerWorld world, Entity entity, @Nullable EquipmentSlot slot) {
+    public void inventoryTick(ItemStack stack, World world, Entity entity, int slot, boolean selected) {
         if (world.isClient) {
             return;
         }
@@ -44,13 +45,13 @@ public class LuckyBundleItem extends BundleItem {
 
         if (player != null) {
             stack.set(DataComponentTypes.CUSTOM_DATA, component.apply(compound -> {
-                if (!compound.contains(MAX_TIME_TAG) && compound.get(MAX_TIME_TAG) instanceof NbtInt) {
+                if (!compound.contains(MAX_TIME_TAG, NbtElement.INT_TYPE)) {
                     compound.putInt(MAX_TIME_TAG, random.nextBetween(2500, 6100));
                 }
 
-                if (compound.contains(MAX_TIME_TAG) && compound.get(MAX_TIME_TAG) instanceof NbtInt) {
-                    int invTime = compound.getInt(TIME_TAG).orElse(0) + 1;
-                    int maxTimeTag = compound.getInt(MAX_TIME_TAG).orElse(0);
+                if (compound.contains(TIME_TAG, NbtElement.INT_TYPE)) {
+                    int invTime = compound.getInt(TIME_TAG) + 1;
+                    int maxTimeTag = compound.getInt(MAX_TIME_TAG);
 
                     if (invTime >= (maxTimeTag - 100)) {
                         if (invTime % 20 == 0) {
@@ -65,7 +66,41 @@ public class LuckyBundleItem extends BundleItem {
                                 if (itemStack.isOf(net.minecraft.item.Items.GHAST_TEAR)) {
                                     hasGhastTear = true;
                                     if (random.nextBoolean()) {
+                                        ItemStack diamond = new ItemStack(net.minecraft.item.Items.DIAMOND, 16);
+                                        ItemStack netheriteIngot = new ItemStack(net.minecraft.item.Items.NETHERITE_INGOT, 4);
+                                        ItemStack netherStar = new ItemStack(net.minecraft.item.Items.NETHER_STAR);
+                                        ItemStack beacon = new ItemStack(net.minecraft.item.Items.BEACON);
+                                        ItemStack loot = new ItemStack(net.minecraft.item.Items.STICK);
+                                        ItemStack loot2 = new ItemStack(net.minecraft.item.Items.LEAD);
+                                        ItemStack loot3 = new ItemStack(net.minecraft.item.Items.STRING);
+                                        if (random.nextBoolean()) {
+                                            loot = diamond;
+                                        } else {
+                                            if (random.nextBoolean()) {
+                                                loot = diamond;
+                                                loot2 = netherStar;
+                                            } else {
+                                                if (random.nextBoolean()) {
+                                                    loot = netheriteIngot;
+                                                    loot2 = beacon;
+                                                } else {
+                                                    loot = diamond;
+                                                    loot2 = netheriteIngot;
+                                                    loot3 = netherStar;
+                                                }
+                                            }
+                                        }
 
+                                        ItemStack droppedStack = new ItemStack(Items.USED_LUCKY_BUNDLE);
+                                        BundleContentsComponent updatedBundleContentsComponent = droppedStack.getOrDefault(DataComponentTypes.BUNDLE_CONTENTS, new BundleContentsComponent(new ArrayList<>()));
+                                        List<ItemStack> updatedItems = new ArrayList<>();
+                                        updatedBundleContentsComponent.iterate().forEach(updatedItems::add);
+                                        updatedItems.add(loot);
+                                        updatedItems.add(loot2);
+                                        updatedItems.add(loot3);
+                                        droppedStack.set(DataComponentTypes.BUNDLE_CONTENTS, new BundleContentsComponent(updatedItems));
+                                        player.sendMessage(Text.of("<LuckyBundle> What a Luck!"), false);
+                                        entity.dropStack((ServerWorld) world, droppedStack);
                                     } else {
                                         if (random.nextBoolean()) {
                                             ItemStack diamondBlock = new ItemStack(net.minecraft.item.Items.DIAMOND, 8);
